ebee49dae4bbb9799999a4ce154f817ecbb83302
SaintRocco <94902268+SaintRocco@users.noreply.github.com>
2024-01-19 21:52:57 +0100

    Account creation

--- Adaptivis_test.spec.js
+++ Adaptivis_test.spec.js
@@ -1,8 +1,18 @@
 const {test, expect} = require('@playwright/test');
 
-test('Account Creation', async ({page}) => {
+test('Initial test', async ({page}) => {
     
+   //initial test
    await page.goto('https://magento.softwaretestingboard.com/');
    await expect(page).toHaveTitle('Home Page');
 
+});
+
+test('Account creation', async ({page}) => {
+
+   //Check Account creation with valild data
+   await page.goto('https://magento.softwaretestingboard.com/');
+
+   //Click the Create an Account link
+   await page.getByText('Create an Account').click();
 })
