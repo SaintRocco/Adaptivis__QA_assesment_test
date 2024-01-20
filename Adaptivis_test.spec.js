b01343871af2abdbc7761e8e8992859292af243a
SaintRocco <94902268+SaintRocco@users.noreply.github.com>
2024-01-20 15:40:39 +0100

    Final commit

--- Adaptivis_test.spec.js
+++ Adaptivis_test.spec.js
@@ -14,8 +14,35 @@ test('Account creation', async ({page}) => {
    await page.goto('https://magento.softwaretestingboard.com/');
 
    //Click the Create an Account link
-   await page.getByText('Create an Account').click();
+   await page.locator('Create an Account').first().click();
 
    //Fill in the First Name
-   await page.getBy
+   await page.getByLabel('firstname').fill("John");
+
+   //Fill in the Last Name 
+   await page.getByLabel('lastname').fill('Smith');
+
+   //Fill in the Email
+   await page.getByLabel('email').fill('jsmith@gmail.com');
+
+   //Fill in the Password and Comfim Password
+   await papge.getByLabel('password').fill('Thestrongestpassword3');
+   await papge.getByLabel('password-confirmation').fill('Thestrongestpassword3');
+
+   //Click the Create an Account button option
+   await page.getByText('Create an Account').click();
+});
+
+
+test('Item selection', async ({page}) => {
+
+   //Check item selection
+   await page.goto('https://magento.softwaretestingboard.com/');
+
+   //Mouse hover over the item
+   await page.getByAltText('Fusion Backpack').hover();
+
+   //Mouse click on the item
+   await page.getByAltText('Fusion Backpack').click();
+
 });
